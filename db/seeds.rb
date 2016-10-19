60a3bfbe38de31d6942342aca689015b55cdac4c
rezn5447 <rezn5447@gmail.com>
2016-10-18 21:41:50 -0700

    final migrations

---
+++ db/seeds.rb
@@ -0,0 +1,9 @@
+
+ MVP.create(title: "Colonel", first_name: "Rando", nick_name: "Action Jackson", last_name: "Soldier", kills: "50", commander_id: 1)
+
+ MVP.create(title: "Major", first_name: "Arnold", nick_name: "Terminator", last_name: "Schwarznegger", kills: "25", commander_id: 1)
+
+ MVP.create(title: "Rookie", first_name: "Fatima", nick_name: "Mama", last_name: "Jawhir", kills: "1", commander_id: 2)
+
+ MVP.create(title: "Rookie", first_name: "Laura", nick_name: "Seer", last_name: "Jackson", kills: "2", commander_id: 2)
+
